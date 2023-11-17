1d38d5d0757aa4a4940446c969eac99c016ca84e
benjaminAouzir <benjamin.aouzir1413@gmail.com>
2023-11-17 15:52:48 +0100

    steps 1,2,3 in P2 + new cleaning

--- cleaning.py
+++ cleaning.py
@@ -4,30 +4,10 @@ import dataframes as RAW
 import pandas as pd
 import numpy as np
 
-# add archetype names to the character metadata
+# Clean movie data
 
-archetypes = [
-    'Hero', 'Anti-Hero', 'Villain', 'Mentor', 'Sidekick', 'Love Interest',
-    'Herald', 'Threshold Guardian', 'Shapeshifter', 'Trickster',
-    'Damsel in Distress', 'Ally', 'Outlaw', 'Innocent', 'Jester', 'Shadow'
-]
+clean_movie_data = pd.read_csv("../Clean data/clean_movie_data.csv")
 
-character_data_ft_archetypes = RAW.character_data.copy()
+# Clean character data
 
-for archetype in archetypes:
-    character_data_ft_archetypes[archetype] = 0
-
-# filter for NaN character names
-
-character_data_ft_archetypes = character_data_ft_archetypes.dropna(subset=['Character name'])
-
-# merge the character and summary dataframes on the Wiki ID column
-
-character_data_ft_archetypes['Wiki ID'] = character_data_ft_archetypes['Wiki ID'].astype(str)
-RAW.summaries['Wiki ID'] = RAW.summaries['Wiki ID'].astype(str)
-
-character_data_ft_summaries = character_data_ft_archetypes.merge(RAW.summaries, on='Wiki ID', how='left')
-
-# filter for NaN valued summaries
-
-character_data_ft_summaries = character_data_ft_summaries.dropna(subset=['Summary'])
+clean_character_data = pd.read_csv("../Clean data/clean_character_data.csv")
